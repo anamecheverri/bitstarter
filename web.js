b882736fc7abba9c80bd36b54ae756e6b6163aad
ana echeverri <ame@anamariaecheverri.com>
2013-07-11 02:00:40 +0000

    c

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var texto = fs.readFileSync('index.html').tostring();
+var texto = fs.readFileSync('index.html','utf-8');
 
 app.get('/', function(request, response) {
  response.send(texto);
